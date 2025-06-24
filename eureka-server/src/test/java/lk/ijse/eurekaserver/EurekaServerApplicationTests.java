bc57abb85f1136b89e1a25ac3e39b2357d68c8da
NipunPrabushitha <nprabushitha@gmail.com>
2025-06-24 20:17:48 +0530

    give full update project

--- eureka-server/src/test/java/lk/ijse/eurekaserver/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/lk/ijse/eurekaserver/EurekaServerApplicationTests.java
@@ -1,9 +1,11 @@
-package lk.ijse.eureka_server;
+package lk.ijse.eurekaserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootTest
+@EnableEurekaServer
 class EurekaServerApplicationTests {
 
     @Test
